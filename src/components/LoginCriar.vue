2cd3200bba1e6488a4e07cbbc870f26d400c3382
fellipebastos <fellipebastoswm@gmail.com>
2021-11-10 19:43:28 -0300

    feat: criacao de usuario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -7,7 +7,9 @@
         Criar conta
       </button>
       <UsuarioForm v-else>
-        <button class="btn btn-form">Criar Usuário</button>
+        <button class="btn btn-form" @click.prevent="criarUsuario">
+          Criar Usuário
+        </button>
       </UsuarioForm>
     </transition>
   </section>
@@ -25,6 +27,21 @@ export default {
     return {
       criar: false,
     };
+  },
+  methods: {
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
+        await this.$store.dispatch(
+          "getUsuario",
+          this.$store.state.usuario.email
+        );
+
+        this.$router.push({ name: "usuario " });
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
